72f68a5303cb908f92d2c3ba88f3229a7be2a9d4
mesepith <zahiralam29@gmail.com>
2024-03-09 16:36:40 +0530

    minor change

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,7 +1,7 @@
 // services/AuthService.ts
 import axios from 'axios';
 
-const API_URL = 'http://172.16.5.95:3001/api'; // Update this with your actual API URL
+const API_URL = 'http://172.20.10.9:3001/api'; // Update this with your actual API URL
 
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
